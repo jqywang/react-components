35082ba64545acfc867dc0fa9abbb596909026bc
jqywang <jqywang.95@gmail.com>
2018-01-31 11:41:54 -0800

    instantiate new react classes and post to index.html

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 // TODO
-var GroceryListItem = (props) => {
-  var onListItemClick = (event) => {
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  onListItemClick (event){
     console.log(this);
-  };
-  return (<div>
-      <ul>
-        <li onClick={onListItemClick} >{props.food[0]}</li>
-        <li>{props.food[1]}</li>
-        <li>{props.food[2]}</li>
-        <li>{props.food[4]}</li>
-        <li>{props.food[3]}</li>
-      </ul>
-    </div>)
+  }
+  render(){
+    return (
+      <li>{this.props.food}</li>
+    )
+  }
 };
-var HypeGrocList = () => (
-  <div>
+var HypeGrocList = (props) => (
+  <ul>
     <h2>TJ's trip goals</h2>
-    <GroceryListItem food={groceryItems}/> 
-  </div>
+    {props.food.map(food => <GroceryListItem food = {food}/>)} 
+  </ul>
 );
 var groceryItems = ['Peanut Butter', 'Jelly', 'Bread', 'Alcohol', 'Bananas'];
 
@@ -35,36 +34,44 @@ var groceryItems = ['Peanut Butter', 'Jelly', 'Bread', 'Alcohol', 'Bananas'];
 //   <li> Jelly </li>
 // );
 
-ReactDOM.render(<HypeGrocList />, document.getElementById("app"));
+ReactDOM.render(<HypeGrocList food={groceryItems}/>, document.getElementById("app"));
 
 
 /*
 
-var TodoList = (props) => {
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
+class TodoListItem extends React.Component {
 
-  // This function will be called when the first `<li>` below is clicked on
-  // Notice that event handling functions receive an `event` object
-  // We want to define it where it has access to `props`
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+  }
 
-  var onListItemClick = (event) => {
-    console.log('I got clicked');
-  };
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+  render() {
 
-  // Because we used curly braces with this arrow function
-  // we have to write an explicit `return` statement
-  return (
-    <ul>
-      <li onClick={onListItemClick}>{props.todos[0]}</li>
-      <li>{props.todos[1]}</li>
-      <li>{props.todos[2]}</li>
-    </ul>
-  );
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+      <li>{this.props.todo}</li>
+    );
+
+  }
+
 }
 
-var App = () => (
-  <div>
-    <h2>My Todo List</h2>
-    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-  </div>
+// Update our `TodoList` to use the new `TodoListItem` component
+// This can still be a stateless function component!
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
 );
 */
